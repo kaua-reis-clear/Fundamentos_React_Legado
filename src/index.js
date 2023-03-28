21881b1b9794033aae5499c76dbe963b86f154a8
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-28 10:30:27 -0300

    created multiple components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
+import { CompA, CompB } from './components/DoisComponentes'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
+        <CompA valor="Olá eu sou o A!"/>
+        <CompB valor="B na área!"/>
         <PrimeiroComponente valoe="Bom dia"/>
     </div>    
 , elemento)
