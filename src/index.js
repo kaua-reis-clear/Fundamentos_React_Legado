0c1aef8966ac7c1741917269d296fb201a918659
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-28 10:20:55 -0300

    initial commit; first component created.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PrimeiroComponente from './components/PrimeiroComponente'
+
+const elemento = document.getElementById('root')
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente/>
+    </div>    
+, elemento)
