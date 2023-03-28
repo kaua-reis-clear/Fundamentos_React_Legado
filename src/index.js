f74da6ec6296b8e5deba810763edba5046a13bea
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-28 10:33:02 -0300

    created multi elements component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
 import { CompA, CompB } from './components/DoisComponentes'
+import MultiElementos from './components/MultiElementos'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
+        <MultiElementos/>
         <CompA valor="Olá eu sou o A!"/>
         <CompB valor="B na área!"/>
         <PrimeiroComponente valoe="Bom dia"/>
