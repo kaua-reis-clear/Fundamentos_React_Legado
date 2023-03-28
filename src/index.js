8e302407b274473db6611d0ca216f23454fa37b3
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-28 10:48:32 -0300

    cretead components comunicating directly

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
 import { CompA, CompB } from './components/DoisComponentes'
 import MultiElementos from './components/MultiElementos'
+import FamiliaSilva from './components/FamiliaSilva'
+import Familia from './components/Familia'
+import Membro from './components/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
+        <Familia sobrenome="Pereira">
+            <Membro nome="André" />
+            <Membro nome="Mariana"/>
+        </Familia>
+        <FamiliaSilva/>
         <MultiElementos/>
         <CompA valor="Olá eu sou o A!"/>
         <CompB valor="B na área!"/>
